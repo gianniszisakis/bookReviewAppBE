dd6462fc77471ce3beb846b0dc0a07cc6e3c213f
Ioannis ZISAKIS <ioannis.zisakis@oracle.com>
2026-01-12 22:23:11 +0200

    fix: Remove wildcard in auth endpoint.

--- final_project/index.js
+++ final_project/index.js
@@ -17,7 +17,7 @@ app.use(
   })
 );
 
-app.use("/customer/auth/*", function auth(req, res, next) {
+app.use("/customer/auth", function auth(req, res, next) {
   if (req.session.authorization) {
     let token = req.session.authorization["accessToken"];
 
