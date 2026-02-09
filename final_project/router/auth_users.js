cbac5fe7abdeb3e6ced02798f1a59a26a2b557d4
Ioannis ZISAKIS <ioannis.zisakis@oracle.com>
2026-02-09 14:06:48 +0200

    feat: Adding or modifying a book review.

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -61,8 +61,25 @@ regd_users.post("/login", (req, res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const review = req.query.review;
+  const isbn = req.params.isbn;
+  const username = req.session.authorization.username;
+
+  if (!review) {
+    return res.status(400).json({ message: "No review" });
+  }
+
+  if (!books[isbn]) {
+    return res.status(400).json({ message: "No book found" });
+  }
+
+  if (!books[isbn].reviews) {
+    books[isbn].reviews = {};
+  }
+
+  books[isbn].reviews[username] = review;
+
+  return res.status(200).json({ message: "review added" });
 });
 
 module.exports.authenticated = regd_users;
